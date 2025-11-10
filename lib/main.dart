2b74fe613a49f79966c08887c8174ca2ae144840
Daksh Bhatia <daksh2210@gmail.com>
2025-11-10 19:17:43 +0530

    Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:provider/provider.dart';
+import 'package:green_sense/config/routes.dart';
+import 'package:green_sense/providers/calculator_provider.dart';
+import 'package:green_sense/providers/quiz_provider.dart';
+import 'package:green_sense/providers/challenge_provider.dart';
+import 'package:green_sense/providers/pledge_provider.dart';
 import 'package:green_sense/constants/app_colors.dart';
-import 'package:green_sense/screens/home_screen.dart';
 
 void main() {
-  runApp(const GreenSenseApp());
+  setPathUrlStrategy();
+  runApp(const MyApp());
 }
 
-class GreenSenseApp extends StatelessWidget {
-  const GreenSenseApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GreenSense',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: AppColors.primaryColor,
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Roboto',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CalculatorProvider()),
+        ChangeNotifierProvider(create: (_) => QuizProvider()),
+        ChangeNotifierProvider(create: (_) => ChallengeProvider()),
+        ChangeNotifierProvider(create: (_) => PledgeProvider()),
+      ],
+      child: MaterialApp.router(
+        title: 'Green Sense',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: AppColors.primaryColor,
+          scaffoldBackgroundColor: AppColors.backgroundGreen,
+          fontFamily: 'Roboto',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: AppColors.primaryColor,
+          ),
+        ),
+        routerConfig: router,
       ),
-      home: const HomeScreen(),
     );
   }
 }
